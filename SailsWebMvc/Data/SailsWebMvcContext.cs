e267849780c29fc8d0b6ed1d2cd08252f4921587
Yuri Carneiro <yuricoelho33@gmail.com>
2022-09-27 19:00:21 -0300

    CRUD Scaffolding

---
+++ SailsWebMvc/Data/SailsWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SailsWebMvc.Models;
+
+namespace SailsWebMvc.Data
+{
+    public class SailsWebMvcContext : DbContext
+    {
+        public SailsWebMvcContext (DbContextOptions<SailsWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SailsWebMvc.Models.Department> Department { get; set; } = default!;
+    }
+}
--- SailsWebMvc/Program.cs
+++ SailsWebMvc/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SailsWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SailsWebMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SailsWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SailsWebMvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
